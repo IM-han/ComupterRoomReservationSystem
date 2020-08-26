e3a68b07b2cc9cd9c23ffaf9a54017457ac20413
IM <854520471@qq.com>
2020-08-26 13:21:20 +0800

    添加项目文件。

---
+++ student.h
@@ -0,0 +1,47 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include "identity.h"
+#include "computerRoom.h"
+#include<vector>
+#include<fstream>
+#include "globalFile.h"
+#include "orderFile.h"
+
+/*学生类的主要功能
+* 显示学生操作的菜单界面
+* 申请预约
+* 查看自身预约
+* 查看所有预约
+* 取消预约
+*/
+
+class Student : public Identity {
+public:
+	//默认构造
+	Student();
+
+	//有参构造（学号，姓名，密码）
+	Student(int id, string name, string pwd);
+
+	//菜单界面
+	virtual void operMenu();
+
+	//申请预约
+	void applyOrder();
+
+	//查看我的预约
+	void showMyOrder();
+
+	//查看所有预约
+	void showAllOrder();
+
+	//取消预约
+	void cancelOrder();
+
+	//学生学号
+	int m_Id;
+
+	//机房容器
+	vector<ComputerRoom> vCom;
+};
